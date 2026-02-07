41403d7a0d482ef04b93cf2e948b56fd713c7b1b
Imran khan <iscosmeticsmultan@gmail.com>
2026-02-07 13:55:25 +0500

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+const supabaseUrl = 'https://trjtmgnkgpbrjijkphqz.supabase.co';
+const supabaseKey = 'sb_publishable_e7m-VJFRacE4mao8Vw5Edw_mB8dnQAK';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase Configured!");
